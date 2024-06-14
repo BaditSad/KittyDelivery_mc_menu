3086018336d40aef31f1e98b272e148247d1463f
BaditSad <bdumortier0@gmail.com>
2024-06-14 12:59:05 +0200

    docker init env

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
+require('dotenv').config();
+
 const menusRouter = require("./controllers/MenuController");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const app = express();
-const port = 3008;
+const port = process.env.PORT || 3008;
 
 app.use(cors());
-
-const db = require("./models");
-db.mongoose
-  .connect(db.url)
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-
-//Ici on envoit les infos vers le front
-
 app.use(bodyParser.json());
+
+// Connexion à MongoDB avec Mongoose
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("Connected to MongoDB!");
+})
+.catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
+  process.exit();
+});
 
 app.use("/", menusRouter);
 
-app.listen(port, () => console.log(`app running on http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
