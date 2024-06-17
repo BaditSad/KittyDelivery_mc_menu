7fcdb89d7e3df1945f6f15ffb30101cf98f67904
NakWalid <walid.nakmouche@viacesi.fr>
2024-06-17 15:30:47 +0200

    migration des menus

--- migration.js
+++ migration.js
@@ -59,6 +59,52 @@ db.once("open", async function () {
         menu_description:
           "Un menu qui vous transporte dans des contrées lointaines",
       },
+      {
+        restaurant_id: 1,
+        menu_name: "barquette de frite",
+        menu_description: "barquette de frite",
+        menu_price: 5,
+        article_list: [ "barquette de frite" ],
+        Menu_image: "/storage/1718575365084-.png",
+      },
+      {
+        restaurant_id: 1,
+        menu_name: "MMenu BIGMAC",
+        menu_description: "sauce mayo",
+        menu_price: 12,
+        article_list: ["Burger + Frite"
+        ],
+        Menu_image: "/storage/1718575740481-.png",
+      },
+      {
+
+        restaurant_id: 1,
+        menu_name: "pizza",
+        menu_description: "pizza + barquette",
+        menu_price: 12,
+        article_list: [ "pizza , barquette"  ],
+        Menu_image: "/storage/1718625353079-.jpg",
+      },
+      {
+        restaurant_id: 1,
+        menu_name: "kwdckw",
+        menu_description: "hkbhkbhk",
+        menu_price: 77,
+        article_list: [
+          "hvkvkh"
+        ],
+        Menu_image: "/storage/1718626028592-.jpg",
+      },
+      {
+        restaurant_id: 1,
+        menu_name: "qdkqw kd",
+        menu_description: "nk kjijn",
+        menu_price: 12,
+        article_list: [
+          "hbhubuhb"
+        ],
+        Menu_image: "/storage/1718628578094-.png",
+      },
     ];
 
     await Menu.insertMany(menus);
