59508a74950da62a364aa09a6bcdb401ef465cd1
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-19 21:29:09 +0200

    feature(setup swagger)

--- server.js
+++ server.js
@@ -1,33 +1,44 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const menusRouter = require("./controllers/MenuController");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const path = require("path"); 
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3008;
+const port = process.env.PORT;
+const mongoURI = process.env.MONGO_URI;
+
+const swaggerDocumentd = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "swagger.json"))
+);
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connexion à MongoDB avec Mongoose
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Connected to MongoDB!");
-})
-.catch((err) => {
-  console.error("Error connecting to MongoDB:", err);
-  process.exit();
+app.use("/swagger.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerDocumentd);
 });
 
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.use("/storage", express.static(path.join(process.cwd(), "./storage"))); 
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit();
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/storage", express.static(path.join(process.cwd(), "./storage")));
 app.use("/", menusRouter);
 
 app.listen(port, () => {
